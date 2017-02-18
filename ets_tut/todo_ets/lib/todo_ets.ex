c934440db0739cf11fcbce53fcd8e15516c2b26f
Woodson Delhia <woodsondelhia88@gmail.com>
2017-02-18 20:12:18 +0900

    Added todos with ets in testing file

--- ets_tut/todo_ets/lib/todo_ets.ex
+++ ets_tut/todo_ets/lib/todo_ets.ex
@@ -1,18 +1,38 @@
 defmodule TodoEts do
-  @moduledoc """
-  Documentation for TodoEts.
-  """
+  use GenServer, async: true
 
-  @doc """
-  Hello world.
+  def start_link, do: GenServer.start(__MODULE__, :ok, name: __MODULE__)
 
-  ## Examples
+  def init(:ok), do: {:ok, :ets.new(:todos, [:named_table, :public]) }
 
-      iex> TodoEts.hello
-      :world
+  def new(name) when is_atom(name), do: GenServer.call(__MODULE__, {:new, name})
 
-  """
-  def hello do
-    :world
+  def add(name, item) when is_binary(item), do: GenServer.call(__MODULE__, {:add, name, item})
+
+  def find(name) when is_atom(name) do
+    case :ets.lookup(:todos,name) do
+      [{^name, items}] -> {:ok, items}
+      [] -> :error
+    end
   end
+
+  def handle_call({:new, name}, _from, table) do
+    case find(name) do
+      {:ok, _} -> {:reply, :already_exist, table}
+      :error ->
+        :ets.insert(table, {name, []})
+        {:reply, [], table}
+    end
+  end
+
+  def handle_call({:add, name, item}, _from, table) do
+    case find(name) do
+      {:ok, items} ->
+        :ets.insert(:todos, {name,[item]++items})
+        {:reply, {:added, items}, table}
+      :error ->
+        {:reply, {:error, :no_list_found}, table}
+    end
+  end
+
 end
--- ets_tut/todo_ets/test/todo_ets_test.exs
+++ ets_tut/todo_ets/test/todo_ets_test.exs
@@ -2,7 +2,28 @@ defmodule TodoEtsTest do
   use ExUnit.Case
   doctest TodoEts
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    first_table = TodoEts.start_link
+    TodoEts.new(:shopping)
+    {:ok, table: first_table}
+  end
+
+  test "ETS todo table created at start of gen server." do
+    assert :ets.all |> Enum.member?(:todos)
+  end
+
+  test "ETS new task" do
+    assert TodoEts.find(:random)
+    assert TodoEts.new(:shopping) == :already_exist
+  end
+
+  test "ETS find method" do
+     assert TodoEts.find(:shopping)
+     assert TodoEts.find(:random) == :error
+  end
+
+  test "ETS add todo item" do
+    assert {:added, _items } = TodoEts.add(:shopping, "milk")
+    assert {:error, :no_list_found} = TodoEts.add(:random, "milk") 
   end
 end
